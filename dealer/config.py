30a66ac3320b908ebf520e31477cefec75a233ef
Radek Jankiewicz <radoslaw.jankiewicz@gmail.com>
2014-11-09 23:30:27 +0100

    players

--- dealer/config.py
+++ dealer/config.py
@@ -7,50 +7,45 @@ SHOW_HAND_CARDS = True
 
 PLAYERS = {
     'player1': {
-        'name': 'Styl Pijanego Napierdalaki',
+        'name': 'Player 1',
         'account': START_ACCOUNT,
         'address': 'localhost:4000',
     },
     'player2': {
-        'name': 'Imbazaury',
+        'name': 'Player 2',
         'account': START_ACCOUNT,
-        'address': 'localhost:4000',
+        'address': 'localhost:4001',
     },
     'player3': {
-        'name': 'Mistrzowie Kolonii 2014',
+        'name': 'Player 3',
         'account': START_ACCOUNT,
-        'address': 'localhost:4000',
+        'address': 'localhost:4002',
     },
     'player4': {
-        'name': 'Salmandia',
+        'name': 'Player 4',
         'account': START_ACCOUNT,
-        'address': 'localhost:4000',
+        'address': 'localhost:4003',
     },
-    'player5': {
-        'name': 'Power Mergers!',
-        'account': START_ACCOUNT,
-        'address': 'localhost:4000',
-    },
-    'player6': {
-        'name': 'ssh localhost',
-        'account': START_ACCOUNT,
-        'address': 'localhost:4000',
-    },
-    'player7': {
-        'name': 'Avangersi STXa',
-        'account': START_ACCOUNT,
-        'address': 'localhost:4000',
-    },
-    'player8': {
-        'name': 'Hot-dog Team',
-        'account': START_ACCOUNT,
-        'address': 'localhost:4000',
-    },
-    'player9': {
-        'name': 'PYrates',
-        'account': START_ACCOUNT,
-        'address': 'localhost:4000',
-    },
+    # 'player5': {
+    #     'name': 'Player 5',
+    #     'account': START_ACCOUNT,
+    #     'address': 'localhost:4000',
+    # },
+    # 'player6': {
+    #     'name': 'Player 6',
+    #     'account': START_ACCOUNT,
+    #     'address': 'localhost:4000',
+    # },
+    # 'player7': {
+    #     'name': 'Player 7',
+    #     'account': START_ACCOUNT,
+    #     'address': 'localhost:4000',
+    # },
+    # 'player8': {
+    #     'name': 'Player 8',
+    #     'account': START_ACCOUNT,
+    #     'address': 'localhost:4000',
+    # },
 }
 
 # uncomment to use save
